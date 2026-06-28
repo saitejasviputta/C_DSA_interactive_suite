d9fae98a96dab6a309fe268cb3afa7a9176f8bc2
Pratikk404 <kalambepratik29@gmail.com>
2026-06-28 12:38:58 +0000

    feat(graph-traversals): clear screen and show a header before each sub-demo

--- src/graph_traversals/graph_traversals_demo.c
+++ src/graph_traversals/graph_traversals_demo.c
@@ -1,3 +1,4 @@
+#include "display_header.h"
 #include "graph_traversals.h"
 #include "safe_input.h"
 #include <stdio.h>
@@ -39,36 +40,47 @@ void graph_traversals_demo(void)
         switch (graph_traversal_choice)
         {
             case 1:
+                display_header("BFS");
                 bfs_demo();
                 break;
             case 2:
+                display_header("DFS");
                 dfs_demo();
                 break;
             case 3:
+                display_header("Dijkstra");
                 dijkstra_demo();
                 break;
             case 4:
+                display_header("A*");
                 astar_demo();
                 break;
             case 5:
+                display_header("Greedy Best-First Search");
                 greedy_best_first_search_demo();
                 break;
             case 6:
+                display_header("Bellman-Ford");
                 bellman_ford_demo();
                 break;
             case 7:
+                display_header("Topological Sort");
                 topological_sort_demo();
                 break;
             case 8:
+                display_header("Visualize Graph");
                 visualize_graph_demo();
                 break;
             case 9:
+                display_header("Kruskal MST");
                 kruskal_demo();
                 break;
             case 10:
+                display_header("Prim MST");
                 prim_demo();
                 break;
             case 11:
+                display_header("Floyd-Warshall");
                 floyd_warshall_demo();
                 break;
         }
